fa2453110a817be6acce7395b9787a68b9c6b2fc
KIA <mojtabahosseinikia@gmail.com>
2024-09-16 17:46:20 +0330

    main

--- src/utils/remark-modified-time.mjs
+++
@@ -1,9 +0,0 @@
-import { statSync } from "fs";
-
-export function remarkModifiedTime() {
-  return function (_tree, file) {
-    const filepath = file.history[0];
-    const result = statSync(filepath);
-    file.data.astro.frontmatter.lastModified = result.mtime.toISOString();
-  };
-}
